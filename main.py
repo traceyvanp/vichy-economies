ba8ef250d30b3943c9147fa3d8a838b7b37013e3
traceyvanp <traceyvanp@gmail.com>
2024-10-10 14:55:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 # Path to ChromeDriver
 driver_path = '/usr/local/bin/chromedriver'  # Ensure chromedriver is installed and the path is correct
@@ -25,10 +26,7 @@ time.sleep(10)
 # Find the required elements
 username_field = driver.find_element(By.XPATH, "*//*[@id='usr_mail']")
 password_field = driver.find_element(By.XPATH, "*//*[@id='passwd']")
-#relative XPATH //input[@id='btn_login']
 connection_button = driver.find_element(By.XPATH, "*//*[@id='btn_login']")
-#print(username_field)
-#print(password_field)
 
 #set the key values
 username_field.send_keys('thomas.van-puyvelde@axereal-elevage.com')
@@ -38,7 +36,7 @@ password_field.send_keys('FeedAx2024')
 connection_button.send_keys(Keys.ENTER)
 
 # Wait for login to complete
-time.sleep(10)  # Adjust based on your network speed and website response time
+time.sleep(10)
 
 # Now navigate to the secured page
 driver.get('https://www.feed-alliance.fr/index.php?option=com_faweb&innerview=terme&innerlayout=main#')
@@ -54,7 +52,7 @@ livres_link.click()
 time.sleep(5)
 
 #navigate to usine
-usine_button = driver.find_element(By.XPATH, "*//span[normalize-space()='AXEREAL ELEVAGE USINES']")
+usine_button = driver.find_element(By.XPATH, "*//span[normalize-space()='TELLUS USINES']")
 usine_button.click()
 time.sleep(5)
 
@@ -67,12 +65,8 @@ time.sleep(5)
 secured_page_content = driver.page_source
 
 # Parse with BeautifulSoup
-from bs4 import BeautifulSoup
 secured_page_soup = BeautifulSoup(secured_page_content, 'html.parser')
 secured_page_soup.prettify()
-
-# Do your scraping here
-#print(secured_page_soup.prettify())
 
 # Initialize an empty list to hold the data
 data = []
@@ -83,7 +77,8 @@ headers = secured_page_soup.find_all('h1')
 
 # Iterate over each header
 for header in headers:
-    header_text = header.get_text()  # Get the text of the current header
+    # Get the text of the current header
+    header_text = header.get_text()  
     
     # Find the next 'tr' elements after the header
     header_row = header.find_next('tr', class_='header_fa')
@@ -96,15 +91,18 @@ for header in headers:
     # Extract prices from the price row
     prices = price_row.find_all('td', class_='borduredroite_bas centrer')
     est_prices = price_row.find_all('td', class_='borduredroite_bas centrer prix_estimes')
+    #combine the current and forecast prices
     combined_prices = prices + est_prices
     price_texts = [price.get_text() for price in combined_prices]
 
     # Combine the data for the current header
+    today = datetime.today().strftime('%Y-%m-%d')
     for date, price in zip(date_texts, price_texts):
         data.append({
             'Header': header_text,
             'Date': date,
-            'Price': price
+            'Price': price,
+            'LastModifiedDate': today
         })
 
 # Create a DataFrame from the data
@@ -114,8 +112,7 @@ df = pd.DataFrame(data)
 print(df)
 
 #Export DataFrame to CSV
-today = datetime.today().strftime('%Y-%m-%d')
-filename = f'axereal_data_{today}.csv'
+filename = 'tellus_data.csv'
 df.to_csv(filename, index=False)
 
 # Close the browser
